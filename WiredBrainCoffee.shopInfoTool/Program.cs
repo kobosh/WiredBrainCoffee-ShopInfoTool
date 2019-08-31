f7e8863418f55e83688e9bb0c00b1b00d44a31a9
kobosh <kokobosh@gmail.com>
2019-08-30 22:13:45 -0400

    add project

---
+++ WiredBrainCoffee.shopInfoTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCoffee.shopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello brain coffe shop ttool!");
+        }
+    }
+}
